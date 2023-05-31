6267065b365acd1a883f3ff8b0a2789b12135529
Afra Zareen <afrazareen04@gmail.com>
2023-05-31 13:42:32 +0400

    Update shop.js

--- shop.js
+++ shop.js
@@ -1,12 +1,12 @@
-let cartIcon = document.querySelector('#cart-icon')
-let cart = document.querySelector('.carttts')
-let closeCart = document.querySelector('#close-menu')
+let cartttsIcon = document.querySelector('#cart-icon')
+let carttts = document.querySelector('.carttts')
+let closemenu = document.querySelector('#close-menu')
 
-cartIcon.onclick = () => {
-    cart.classList.add("active");
+cartttsIcon.onclick = () => {
+    carttts.classList.add("active");
 }
-closeCart.onclick = () => {
-    cart.classList.remove("active");
+closemenu.onclick = () => {
+    carttts.classList.remove("active");
 }
 if (document.readyState == 'loading'){
     document.addEventListener('DOMContentLoaded' , ready);
@@ -18,20 +18,20 @@ function ready(){
     console.log(removeCartButtons)
     for (var i = 0; i < removeCartButtons.length; i++){
         var button = removeCartButtons[i]
-        button.addEventListener('click' , removeCartItem)
+        button.addEventListener('click' , removeCartIteminCart)
     }
     var quantityInputs = document.getElementsByClassName('carttts-quantity')
     for (var i = 0; i < quantityInputs.length; i++){
         var input = quantityInputs[i];
-        input.addEventListener("change" , quantityChanged);
+        input.addEventListener("change" , quantityChangedinCart);
     }
 }
-function removeCartItem(event){
+function removeCartIteminCart(event){
     var buttonClicked = event.target;
     buttonClicked.parentElement.remove();
     updatetotal()
 }
-function quantityChanged(event){
+function quantityChangedinCart(event){
     var input = event.target
     if (isNaN(input.value) || input.value <= 0){
         input.value = 1
@@ -39,13 +39,13 @@ function quantityChanged(event){
     updatetotal();
 }
 function updatetotal(){
-    var cartContent = document.getElementsByClassName('carttts-cont')[0];
-    var cartBoxes = cartContent.getElementsByClassName('carttts-box');
+    var cartttsContent = document.getElementsByClassName('carttts-cont')[0];
+    var cartttsBoxes = cartttsContent.getElementsByClassName('carttts-box');
     var total = 0;
-    for (var i = 0; i < cartBoxes.length; i++){
-        var cartBox = cartBoxes[i];
-        var priceElement = cartBox.getElementsByClassName('carttts-price')[0];
-        var quantityElement = cartBox.getElementsByClassName('carttts-quantity')[0];
+    for (var i = 0; i < cartttsBoxes.length; i++){
+        var cartttsBox = cartttsBoxes[i];
+        var priceElement = cartttsBox.getElementsByClassName('carttts-price')[0];
+        var quantityElement = cartttsBox.getElementsByClassName('carttts-quantity')[0];
         var price = parseFloat(priceElement.innerText.replace("AED" , ""));
         var quantity = quantityElement.value;
         total = total + (price * quantity);
